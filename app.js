8ee7e818e6d54c0e083aa4530329a86cf518cd7e
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2024-09-12 17:58:15 +0700

    up

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+// const chalk = require('chalk') change color
+const debug = require('debug')('app') //ดูว่าเกิดเหตุการณ์อะไรขึ้นบ้าง
+const morgan = require('morgan') // ดูว่าอุปกรณ์ไหนเข้ามาทำอะไร
+
+const app = express()
+const port = 3080;
+
+app.use(morgan('combined'))
+
+
+app.get("/",(req,res)=>{
+
+    res.send('Hello Pinmanee')
+
+
+})
+
+app.listen(port, () =>{
+    // console.log('listening on port ' + port)
+    debug('listening on port ' + port)
+})
