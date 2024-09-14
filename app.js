a8b7e42c1793e04c185f8981ba55964bbf1e5835
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2024-09-14 17:02:50 +0700

    .

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express')
 // const chalk = require('chalk') change color
 const debug = require('debug')('app') //ดูว่าเกิดเหตุการณ์อะไรขึ้นบ้าง
 const morgan = require('morgan') // ดูว่าอุปกรณ์ไหนเข้ามาทำอะไร
-const path = require('path')
-const path = require('path')
+const path = require('path'); // Change this line
 const app = express()
 const port = 4800;
 
 app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname,"/public/intex.html")))
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/",(req,res)=>{
 
